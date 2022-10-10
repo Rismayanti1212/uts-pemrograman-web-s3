48874ab4c902306fb91dc18387461844ae5f112e
Rismayanti1212 <chimaa1204@gmail.com>
2022-10-10 17:48:58 +0800

    init

---
+++ aboutme.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="shortcut icon" href="logo.png">
+    <title>Posttest 2 Pemrograman WEB</title>
+    <link rel="stylesheet" href="aboutme.css" />
+</head>
+<body>
+    <div class="container"></div>
+    
+    <header>
+        <nav>
+            <div class = "box-nav" align = "center"> 
+            <ul class = "button-nav">
+                <li><a href="index-user.php">HOME</a></li>
+                <li><a href="#">MENUS</a></li>
+                <li><a href="#">OUR SERVICES</a></li>
+                <li><a href="#">PROMO</a></li>
+                <li><a href="aboutme.php">ABOUT ME</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+            </div>
+        </nav>
+
+        <h1 align = "center"><strong>T H E  C'M Zy   R E S T A U R A N T</strong></h1>
+    </header>
+
+    <div class = "parent-konten1">
+        <div>
+            <h2 align = "center">O U R  S T O R Y</h2>
+            <p class = "border-teks1" align = "center"><mark class = "hightlight">A certified Seminyak hotspot, FIRE has always been the attention for global foodies and those who love some sizzle. Prime meats are delicately dry-aged by our Chefs while waves of fresh seafood are caught locally and imported daily from Down Under. Innovatively prepared ingredients include Tasmanian Salmon and Slow-Cooked Organic Chicken.</mark></p>
+        </div>
+
+        <div>
+            <img class = "img-konten1" src = "img/our-story.jpg">
+        </div>
+    </div>
+
+    <div class = "parent-konten2">
+        <div>
+            <img class = "img-konten2-1" src = "img/owner.jpg" width = 200px height = 200px>
+        </div>
+
+        <div>
+            <img class = "img-konten2-2" src = "img/owner1.jpg" width = 200px height = 200px>
+        </div>
+
+        <div>
+            <h3 align = "center">A B O U T    M E</h3>
+            <p class = "border-teks2" align = "center"><mark class = "hightlight">HI!, My name is Rismayanti, I am a student at Mulawarman University, majoring in informatics. this is my first website The C'MZy Restaurant, come and visit my restaurant there will be a lot of food with a very delicious taste that you can eat with the concept of a luxurious indoor room suitable for you who are elegant</mark></p>
+        </div>
+    </div>
+
+    <div class = "border-reserv" align = "right">
+        <div>
+                <br><br><br><br>
+              RESERVE YOUR TABLE NOW!!!
+            <div class = "button-reserv"><a href="#">BOOKING NOW</a>
+            </div>
+        </div>
+
+        <div class = logo>
+                <a href = "https://instagram.com/chmrsss_?igshid=YmMyMTA2M2Y="><img src = "img/logo-ig.png" width = 40px height = 40px></a><br>
+                <a href = "mailto:chimaa1204@gmail.com"><img src = "img/email.png" width = 40px height = 40px></a><br>
+                <a href = "http://wa.me//6285281877461"><img src = "img/logo-wa.png" width = 40px height = 40px></a><br>
+        </div>
+    </div>
+
+    <footer align="center">
+        <p>Chimaa Owner The C'MZy RESTAURANT<br>
+            Copyright &copy; 2022
+        </p>
+    </footer>
+</body>
+</html>
